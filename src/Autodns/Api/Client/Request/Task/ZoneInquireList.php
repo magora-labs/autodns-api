29fe4a70d1ad81f6faa840db2b2710cffc11ba08
Gerhard Alfanz <gerhard.alfanz@magora.com>
2017-06-01 08:59:23 +0200

    Added ZoneInquireList task

---
+++ src/Autodns/Api/Client/Request/Task/ZoneInquireList.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Autodns\Api\Client\Request\Task;
+
+
+use Autodns\Api\Client\Request\Task;
+
+class ZoneInquireList implements Task
+{
+    /**
+     * @var string[]
+     */
+    private $keys;
+    /**
+     * @var QueryInterface
+     */
+    private $query;
+
+    /**
+     * @var string[]
+     */
+    private $view;
+
+    public function asArray()
+    {
+        $array = array('code' => '0205');
+
+        if ($this->view) {
+            $array['view'] = $this->view;
+        }
+
+        if ($this->keys) {
+            $array['key'] = $this->keys;
+        }
+
+
+        if ($this->query) {
+            $array['where'] = $this->query->asArray();
+        }
+
+        return $array;
+    }
+
+    /**
+     * @param $view
+     * @return $this
+     */
+    public function withView(array $view)
+    {
+        $this->view = $view;
+        return $this;
+    }
+
+    /**
+     * @param array $keys
+     * @return $this
+     */
+    public function withKeys(array $keys)
+    {
+        $this->keys = $keys;
+        return $this;
+    }
+
+    /**
+     * @param QueryInterface $query
+     * @return $this
+     */
+    public function withQuery(QueryInterface $query)
+    {
+        $this->query = $query;
+        return $this;
+    }
+}
